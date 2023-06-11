06c4121f946c2734ea6a81193b307789385b2350
atayiilmaz <ataberkyiilmaz@outlook.com>
2023-06-11 13:32:36 +0300

    challenges completed

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req,res) {
+
+  let ipAddress = req.ip;
+  let pLanguage = req.headers['accept-language'];
+  let pSoftware = req.headers['user-agent'];
+
+  res.json({ 
+    ipaddress: ipAddress,
+    language: pLanguage,
+    software: pSoftware
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
